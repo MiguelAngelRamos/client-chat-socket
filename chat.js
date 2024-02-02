b5b3da709640d89b97e5096dab965158921cf119
Miguel Angel Ramos <return.infox@gmail.com>
2024-02-02 15:40:19 -0300

    refactorizacion del cliente con socket

--- chat.js
+++ chat.js
@@ -8,23 +8,30 @@ const userNameInput = document.querySelector('#user-name');
 
 const roomNameInput = document.querySelector('#room-name');
 const joinRoomButton = document.querySelector('#join-room-button');
+const joinGeneralButton = document.querySelector('#join-general-button');
 
-let currentRoom = ''; // Variable para almacenar la sala actual
+let currentRoom = 'general'; // Variable para almacenar la sala actual
+
+// Unirse a la sala general al cargar
+socket.emit('join general');
 
 joinRoomButton.addEventListener('click', () => {
   currentRoom = roomNameInput.value;
-  if(currentRoom) {
-    socket.emit('leave room', currentRoom); // Salir de la sala acual y si se está en una
-  }
   socket.emit('join room', currentRoom);
-  newMessageTextArea.value = '';
+  messageContainer.innerHTML = ''; // Limpiar el contenedor de mensajes
+});
+
+joinGeneralButton.addEventListener('click', ()=> {
+  currentRoom = 'general';
+  socket.emit('join general');
+  messageContainer.innerHTML = ''; // Limpiar el contenedor de mensajes
 });
 
 sendButton.addEventListener('click', () => {
   const message = newMessageTextArea.value;
   const userName = userNameInput.value || 'Anónimo';
 
-  socket.emit('chat message', {userName, message});
+  socket.emit('chat message', { room: currentRoom, userName, message});
 
   // Limpiar el campo de texto para el prox message
   newMessageTextArea.value = '';
